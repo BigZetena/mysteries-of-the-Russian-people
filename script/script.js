6772a4cb1e22995798ff53cdaa6f299fb660099f
BigZetena <v.o.sharipov@gmail.com>
2023-03-06 22:49:11 +0300

    refactoring script, fix repeat bug

--- script/script.js
+++ script/script.js
@@ -4,155 +4,188 @@ const outsideButton = document.querySelector(".myst__outside");
 const humanButton = document.querySelector(".myst__people");
 const section = document.querySelector(".myst__section");
 
-let homeCopy = home;
-let outsideCopy = outside;
-let humanCopy = human;
+//создаем копии чтобы не мутировать дату
+let homeCurrent = home;
+let outsideCurrent = outside;
+let humanCurrent = human;
 
-let homeCount = -1;
-let homeFailCount = 0;
-let outsideCount = -1;
-let outsideFailCount = 0;
-let humanCount = -1;
-let humanFailCount = 0;
+//создаем массивы с id объектов первоначальных отрисовок
+let homeIdArr = getRandomId(homeCurrent, 6);
+let outsideIdArr = getRandomId(outsideCurrent, 6);
+let humanIdArr = getRandomId(humanCurrent, 6);
 
-drowThemeCounter(homeCopy, true);
-drowThemeCounter(outsideCopy, true);
-drowThemeCounter(humanCopy, true);
+drowCounters(); //первичная отрисовка счетчиков
 
-function drowThemeCounter(arr, param) {
-  let buttonSelector;
-  let i;
-  let f;
-  if (param) {
-    switch (arr) {
-      case homeCopy:
-        homeCount++;
-        break;
-      case outsideCopy:
-        outsideCount++;
-        break;
-      case humanCopy:
-        humanCount++;
-        break;
-    }
-  } else {
-    switch (arr) {
-      case homeCopy:
-        homeFailCount++;
-        break;
-      case outsideCopy:
-        outsideFailCount++;
-        break;
-      case humanCopy:
-        humanFailCount++;
-        break;
-    }
-  }
-  switch (arr) {
-    case homeCopy:
-      buttonSelector = homeButton;
-      f = homeFailCount;
-      i = homeCount;
-      break;
-    case outsideCopy:
-      buttonSelector = outsideButton;
-      f = outsideFailCount;
-      i = outsideCount;
-      break;
-    case humanCopy:
-      buttonSelector = humanButton;
-      f = humanFailCount;
-      i = humanCount;
-      break;
+function getRandomId(arr, allСycles) {
+  let array = [];
+  let result = [];
+  let id;
+
+  for (let i = 0; i <= arr.length; i++) {
+    array.push(i);
   }
 
-  const counter = buttonSelector.querySelector(".myst__counter");
-  const failCounter = buttonSelector.querySelector(".myst__fail");
-  counter.textContent = `${i}/${arr.length} отгадано`;
-  failCounter.textContent = `${f} раз сдался`;
+  for (let countCycles = 1; countCycles <= allСycles; countCycles++) {
+    id = array.splice(Math.random() * (array.length - 1), 1)[0];
+    result.push(id);
+  }
+  console.log(result);
+  for (let countId = 0; countId <= result.length - 1; countId++) {
+    //во время создания id присваеваем объекту ключ, что он уже отрисован
+    arr[result[countId]].display = true;
+  }
+  console.log(result);
+  return result;
 }
 
-function drowCard(arr, card, id) {
-  if (!id) id = Math.floor(Math.random() * arr.length);
+// перерисовка карточек темы
+function drowThemeCards(arr, idArr) {
+  section.innerHTML = "";
+  if (!idArr.length) {
+    console.log("ee");
+    section.innerHTML = `<p class="myst__text">Загадки в теме закончились</p>`;
+  }
+  // находим индекс карточки с нужным нам id
+  let objIdArr = idArr.map((id) => {
+    return arr.findIndex((obj) => obj.id === id);
+  });
+  objIdArr.forEach((id) => {
+    drowCard(arr[id], arr, idArr);
+  });
+  drowCounters();
+}
+
+function drowCounters() {
+  drowThemeCounter(homeCurrent, homeButton);
+  drowThemeCounter(outsideCurrent, outsideButton);
+  drowThemeCounter(humanCurrent, humanButton);
+}
+
+function drowThemeCounter(arr, button) {
+  const i = arr.filter((obj) => obj.done === true).length;
+  const f = arr.filter((obj) => obj.fail === true).length;
+  const last = arr.filter(
+    (obj) => obj.done === false && obj.fail === false
+  ).length;
+  const counter = button.querySelector(".myst__counter");
+  counter.innerHTML = `<p class="myst__counter"">
+                      <span class="myst__counter">${i}</span>
+                      <span class="myst__branch">/</span>
+                      <span class="myst__fail">${f}</span></p>
+                      <p class="myst__branch">${last} Осталось</p>`;
+}
+
+//отрисовка одной карточки
+function drowCard(obj, arr, idArr) {
+  const card = document.createElement("div");
   card.className = "myst__card";
-  card.innerHTML = `
-  <div class="card__info">
-     <p class="card__text">${arr[id].text}</p>
-     <input type="text" class="card__input" placeholder = "Введите ответ" tabindex="-1">
-    </div>
-   <div class="card_navigation">
-      <div class="card__buttons">
-    <button class="card__button__help ">Подсказка</button>
-    <button class="card__button__replace ">Заменить</button>
-   
-  </div> 
-  <div class="card__answer">
-    <button class="card__button__answer ">Сдаешься?</button>
-  </div>   
-    </div>`;
+  card.innerHTML = `<div class="card__info">
+                      <p class="card__text">${obj.text}</p>
+                      <input type="text" class="card__input" placeholder = "Введите ответ" tabindex="-1">
+                      </div>
+                    <div class="card_navigation">
+                        <div class="card__buttons">
+                      <button class="card__button__help ">Подсказка</button>
+                      <button class="card__button__replace ">Заменить</button>
+
+                    </div>
+                    <div class="card__answer">
+                      <button class="card__button__answer ">Сдаешься?</button>
+                    </div>
+                      </div>`;
   const input = card.querySelector(".card__input");
   const help = card.querySelector(".card__button__help");
   const replace = card.querySelector(".card__button__replace");
   const answer = card.querySelector(".card__button__answer");
   const cardButtons = card.querySelector(".card_navigation");
+  //показать подсказку
   help.addEventListener("click", () => {
-    help.textContent = ucFirst(arr[id].help);
+    help.textContent = ucFirst(obj.help);
     help.className = "card__button__help__show";
   });
-
-  replace.addEventListener("click", () => drowCard(arr, card));
+  //замена карточки
+  replace.addEventListener("click", () => {
+    replaceCard(obj, arr, idArr);
+  });
   answer.addEventListener("click", () =>
-    showAnswer(arr, id, cardButtons, card)
+    showAnswer(obj, arr, idArr, cardButtons, card)
   );
   input.addEventListener("change", (event) => {
     event.preventDefault();
-    if (input.value.toLowerCase() === arr[id].answer) {
-      card.innerHTML = `<div class="card__win__div">
+    checkAnswer(obj, arr, idArr, card, event.target.value);
+  });
+  section.append(card);
+  console.log(idArr);
+}
+
+// функция замены карточки
+function replaceCard(obj, arr, idArr) {
+  //заменяемой карточке присваевам ключ, что она уже не отрисована
+  obj.display = false;
+  //находим индекс объекта в дате с нужным нам id в объекте
+  let indexArr = arr.findIndex((elem) => elem === obj);
+  let indexIdArr = idArr.findIndex((elem) => elem === indexArr);
+  //фильтруем дату отсекая завершенные, проваленные, и те что сейчас на экране
+  let arrFiltert = arr.filter(
+    (obj) => obj.done === false && obj.fail === false && obj.display === false
+  );
+  if (!arrFiltert.length) {
+    idArr.splice(indexIdArr, 1);
+    drowThemeCards(arr, idArr);
+    return;
+  }
+  //выбираем id случайной карточки из отфильтрованного массива
+  let id = Math.floor(Math.random() * arrFiltert.length);
+  id = arrFiltert[id].id;
+  //заменяем в массиве id заменяемой карточки на новую
+  idArr.splice(indexIdArr, 1, id);
+  //новой карточке присваевам ключ, что она уже отрисована
+  console.log(idArr);
+  arr[id].display = true;
+  drowThemeCards(arr, idArr);
+}
+
+function checkAnswer(obj, arr, idArr, card, value) {
+  if (value.toLowerCase().trim() === obj.answer) {
+    card.innerHTML = `<div class="card__win__div">
             <p class="card__win__text">Умница!</p>
       </div>`;
-      arrReduse(arr);
-      drowThemeCounter(arr, true);
-      setTimeout(() => drowCard(arr, card), 1500);
-    } else {
-      card.innerHTML = `<div class="card__fail__div">
+    obj.done = true;
+    setTimeout(() => {
+      drowThemeCards(arr, idArr);
+      replaceCard(obj, arr, idArr);
+    }, 1500);
+  } else {
+    card.innerHTML = `<div class="card__fail__div">
             <p class="card__win__text">Неверно!</p>
       </div>`;
-      setTimeout(() => drowCard(arr, card, id), 1500);
-    }
-  });
+    setTimeout(() => drowThemeCards(arr, idArr), 1100);
+  }
 }
 
-function showAnswer(arr, id, cardButtons, card) {
+function showAnswer(obj, arr, idArr, cardButtons) {
   cardButtons.innerHTML = `<p class= "card__button__answer__show">${ucFirst(
-    arr[id].answer
+    obj.answer
   )}</p>`;
-  arrReduse(arr, arr[id].id);
-  drowThemeCounter(arr, false);
-  setTimeout(() => drowCard(arr, card), 2500);
-}
-
-function arrReduse(arr, id) {
-  const index = arr.findIndex((n) => n.id === id);
-  if (index !== -1) {
-    arr.splice(index, 1);
-  }
-}
-
-function drowThemeCard(arr, count) {
-  section.innerHTML = "";
-  for (let i = 0; i < count; i++) {
-    const card = document.createElement("div");
-    drowCard(arr, card);
-    section.append(card);
-  }
+  obj.fail = true;
+  setTimeout(() => replaceCard(obj, arr, idArr), 2500);
 }
 
 function ucFirst(str) {
   if (!str) return str;
-
   return str[0].toUpperCase() + str.slice(1);
 }
 
-homeButton.addEventListener("click", () => drowThemeCard(homeCopy, 6));
-outsideButton.addEventListener("click", () => drowThemeCard(outsideCopy, 6));
-humanButton.addEventListener("click", () => drowThemeCard(humanCopy, 6));
+function showBlock(block) {
+  block.classList.add("b-show");
+}
+
+homeButton.addEventListener("click", () =>
+  drowThemeCards(homeCurrent, homeIdArr)
+);
+outsideButton.addEventListener("click", () =>
+  drowThemeCards(outsideCurrent, outsideIdArr)
+);
+humanButton.addEventListener("click", () =>
+  drowThemeCards(humanCurrent, humanIdArr)
+);
